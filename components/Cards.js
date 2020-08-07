aada92ba8f8f09c6c6aa213f3023f0aebd490db8
git-austinmccollom <austinmccollom@gmail.com>
2020-08-07 11:26:20 -0500

    cardMaker component returning correctly, but no content yet

--- components/Cards.js
+++ components/Cards.js
@@ -4,7 +4,52 @@
 // Study the response data you get back, closely.
 // You will be creating a card for each article in the response.
 // This won't be as easy as just iterating over an array though.
-//
+
+axios.get('https://lambda-times-api.herokuapp.com/articles')
+    .then( response => {
+        console.log(response);
+    })
+
+    .catch( error => {
+        console.log(error);
+        debugger
+    })
+
+function cardMaker( { headline, authorPhoto, authorName } ) {
+
+    // instantiate elements
+    const card = document.createElement('div');
+    const headlineElement = document.createElement('div');
+    const author = document.createElement('div');
+    const imgContainer = document.createElement('div');
+    const image = document.createElement('img');
+    const name = document.createElement('span');
+
+    // append and organize
+
+    card.appendChild(headlineElement);
+    card.appendChild(author);
+    author.appendChild(imgContainer);
+    imgContainer.appendChild(image);
+    author.appendChild(name);
+
+    // dynamically add content
+
+    headlineElement.textContent = headline;
+    image.setAttribute('src', authorPhoto);
+    name.textContent = authorName;
+
+    // style
+
+    card.classList.add('card');
+    headlineElement.classList.add('headline');
+    author.classList.add('author');
+    imgContainer.classList.add('img-container');
+
+    return card;
+}
+console.log(cardMaker({}));
+
 // Write a function that takes a single article object and returns the following markup:
 //
 // <div class="card">
